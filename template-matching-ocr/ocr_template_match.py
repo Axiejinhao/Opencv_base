0f8c5debc39f49bb71dbd9ce8a46f4db175826ea
xjh <xjh9836@gmail.com>
2021-11-22 13:00:54 +0800

    卡号识别(完)

--- template-matching-ocr/ocr_template_match.py
+++ template-matching-ocr/ocr_template_match.py
@@ -13,6 +13,11 @@ ap.add_argument("-t", "--template", required=True,
                 help="path to template OCR-A image")
 args = vars(ap.parse_args())
 
+"""
+--image=images/credit_card_01.png
+--template=images/ocr_a_reference.png
+"""
+
 # 指定信用卡类型
 FIRST_NUMBER = {
     "3": "American Express",
@@ -35,23 +40,26 @@ cv_show('img', img)
 # 灰度图
 ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv_show('ref', ref)
-# 二值图像
+# 二值图像,获取返回值的第二个值
 ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY_INV)[1]
 cv_show('ref', ref)
 
 # 计算轮廓
-# cv2.findContours()函数接受的参数为二值图，即黑白的（不是灰度图）,cv2.RETR_EXTERNAL只检测外轮廓，cv2.CHAIN_APPROX_SIMPLE只保留终点坐标
+# cv2.findContours()函数接受的参数为二值图，即黑白的(不是灰度图)
+# cv2.RETR_EXTERNAL只检测外轮廓，cv2.CHAIN_APPROX_SIMPLE只保留终点坐标
 # 返回的list中每个元素都是图像中的一个轮廓
 
-ref_, refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 cv2.drawContours(img, refCnts, -1, (0, 0, 255), 3)
 cv_show('img', img)
 print(np.array(refCnts).shape)
-refCnts = myutils.sort_contours(refCnts, method="left-to-right")[0]  # 排序，从左到右，从上到下
+# 排序(返回排序后的轮廓)从左到右,从上到下
+refCnts = myutils.sort_contours(refCnts, method="left-to-right")[0]
 digits = {}
 
 # 遍历每一个轮廓
+# i是索引,c是轮廓
 for (i, c) in enumerate(refCnts):
     # 计算外接矩形并且resize成合适大小
     (x, y, w, h) = cv2.boundingRect(c)
@@ -73,13 +81,19 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv_show('gray', gray)
 
 # 礼帽操作，突出更明亮的区域
+# 根据字体大小,选择合适的kernel
 tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
 cv_show('tophat', tophat)
-# 
-gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0,  # ksize=-1相当于用3*3的
-				  ksize=-1)
 
+# ksize=-1相当于用3*3的
+gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
 gradX = np.absolute(gradX)
+
+# gradY = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
+# gradY = np.absolute(gradY)
+#
+# gradXY = cv2.addWeighted(gradX, 0.5, gradY, 0.5, 0)
+
 (minVal, maxVal) = (np.min(gradX), np.max(gradX))
 gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
 gradX = gradX.astype("uint8")
@@ -90,20 +104,18 @@ cv_show('gradX', gradX)
 # 通过闭操作（先膨胀，再腐蚀）将数字连在一起
 gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
 cv_show('gradX', gradX)
+
 # THRESH_OTSU会自动寻找合适的阈值，适合双峰，需把阈值参数设置为0
-thresh = cv2.threshold(gradX, 0, 255,
-                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 cv_show('thresh', thresh)
 
 # 再来一个闭操作
-
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)  # 再来一个闭操作
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
 cv_show('thresh', thresh)
 
 # 计算轮廓
-
-thresh_, threshCnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                                  cv2.CHAIN_APPROX_SIMPLE)
+threshCnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                         cv2.CHAIN_APPROX_SIMPLE)
 
 cnts = threshCnts
 cur_img = image.copy()
@@ -118,9 +130,9 @@ for (i, c) in enumerate(cnts):
     ar = w / float(h)
 
     # 选择合适的区域，根据实际任务来，这里的基本都是四个数字一组
-    if ar > 2.5 and ar < 4.0:
+    if 2.5 < ar < 4.0:
 
-        if (w > 40 and w < 55) and (h > 10 and h < 20):
+        if (40 < w < 55) and (10 < h < 20):
             # 符合的留下来
             locs.append((x, y, w, h))
 
@@ -137,12 +149,12 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
     group = gray[gY - 5:gY + gH + 5, gX - 5:gX + gW + 5]
     cv_show('group', group)
     # 预处理
-    group = cv2.threshold(group, 0, 255,
-                          cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    group = cv2.threshold(group, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     cv_show('group', group)
+
     # 计算每一组的轮廓
-    group_, digitCnts, hierarchy = cv2.findContours(group.copy(), cv2.RETR_EXTERNAL,
-                                                    cv2.CHAIN_APPROX_SIMPLE)
+    digitCnts, hierarchy = cv2.findContours(group.copy(), cv2.RETR_EXTERNAL,
+                                            cv2.CHAIN_APPROX_SIMPLE)
     digitCnts = contours.sort_contours(digitCnts,
                                        method="left-to-right")[0]
 
@@ -160,8 +172,7 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
         # 在模板中计算每一个得分
         for (digit, digitROI) in digits.items():
             # 模板匹配
-            result = cv2.matchTemplate(roi, digitROI,
-                                       cv2.TM_CCOEFF)
+            result = cv2.matchTemplate(roi, digitROI, cv2.TM_CCOEFF)
             (_, score, _, _) = cv2.minMaxLoc(result)
             scores.append(score)
 
@@ -182,3 +193,4 @@ print("Credit Card Type: {}".format(FIRST_NUMBER[output[0]]))
 print("Credit Card #: {}".format("".join(output)))
 cv2.imshow("Image", image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
